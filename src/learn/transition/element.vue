8638e2315b47967f3e1f7cdf2bd8efdfad839838
zhaomin <shs-zhcsyfczyzm@chinaunicom.cn>
2020-12-02 16:02:26 +0800

    transition

---
+++ src/learn/transition/element.vue
@@ -0,0 +1,34 @@
+<template>
+  <transition name="fade">
+    <div v-if="state.ok">toggled content</div>
+  </transition>
+  <button @click="toggle">toggle</button>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+export default {
+  setup() {
+    const state = reactive({ ok: true })
+
+    function toggle() {
+      state.ok = !state.ok
+    }
+
+    return {
+      state,
+      toggle
+    }
+  }
+}
+</script>
+
+<style>
+.fade-enter-from,.fade-leave-to{
+  opacity: 0;
+}
+.fade-enter-active,.fade-leave-active{
+  transition: all 5s linear 0s;
+}
+</style>
